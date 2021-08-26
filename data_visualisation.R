4a60c0f622d556a7d97ac46fa84c5f953f40de82
Your Name <you@example.com>
2021-08-26 21:22:55 +0200

    first plots (overall estimate over all years for all species and estimates per year for each species)

--- data_visualisation.R
+++ data_visualisation.R
@@ -8,7 +8,7 @@ setwd(results_dir)
 library(ggplot2)
 library(brms)
 
-##### plot coefficients for each species ####
+##### plot trend for all species ####
 index_mods<-list.files(".", pattern="index_*")
 names<-gsub("index_","",index_mods)
 names<-gsub(".rds","", names)
@@ -30,32 +30,55 @@ results_dat<-subset(results_dat,!results_dat$species=="Alburnus mento")
 results_dat<-subset(results_dat,!results_dat$species=="Hypophthalmichthys molitrix")
 results_dat<-subset(results_dat,!results_dat$species=="Babka gymnotrachelus")
 
-
-
-
-
-ggplot() +
+p<-ggplot() +
   theme_classic()+
-  geom_pointrange(data=results_dat, mapping=aes(x=species, y=estimate, ymin=Q2.5, ymax=Q9.5))+
+  geom_pointrange(data=results_dat, mapping=aes(x=species, y=estimate, ymin=Q2.5, ymax=Q9.5), size=.8, color="darkgreen")+
   theme(axis.text.x = element_text(angle=90))+
-  geom_hline(yintercept=1, linetype="dashed")
+  geom_hline(yintercept=1, linetype="dashed")+
+  theme(text = element_text(size = 20))
 
+ggsave("C:/Users/zf53moho/Documents/NFDI4BioDiv/Data/Fish Data/Fischdaten_Datenbank/Red_List_Fish_Data/result_visualisation/trend_all_species.png", plot = p, dpi=300)
 
-
-
-
-
+##### plot estimates for each year for each species #####
 
 factor_mods<-list.files(".", pattern="factor_*")
-index_mods<-list.files(".", pattern="index_*")
-factor_mods
+names<-gsub("factor_","",factor_mods)
+names<-gsub(".rds","", names)
 
-tmp_mod_factor<-readRDS(factor_mods[7])
-tmp_mod_index<-readRDS(index_mods[7])
 
-tmp_mod_factor
-fixef(tmp_mod_index)
+#setup parallel backend to use many processors
+library(foreach)
+library(doParallel)
+cores=detectCores()
+cl <- makeCluster(cores[1]) #not to overload your computer
+registerDoParallel(cl)
 
+p<-foreach(i=1:length(factor_mods), .packages=c("ggplot2", "brms")) %dopar% 
+#for (i in 1:length(factor_mods))
+{
+  tmp_mod<-readRDS(factor_mods[i])
+
+  tmp_dat<-as.data.frame(fixef(tmp_mod))
+  tmp_dat<- head(tmp_dat, - 1)
+  year<-rownames(tmp_dat)
+  year<-gsub("factor_year","",year)
+  tmp_dat$year<-year
+  tmp_dat$factor_year<-as.factor(tmp_dat$year)
+  tmp_dat$trans_Estimate<-exp(tmp_dat$Estimate)
+  tmp_dat$trans_Q2.5<-exp(tmp_dat$Q2.5)
+  tmp_dat$trans_Q97.5<-exp(tmp_dat$Q97.5)
+
+  p<-ggplot() +
+    theme_classic()+
+    geom_pointrange(data=tmp_dat, mapping=aes(x=factor_year, y=trans_Estimate, ymin=trans_Q2.5, ymax=trans_Q97.5), size=.8, color="darkgreen") +
+    theme(text = element_text(size = 20))+
+    theme(axis.text.x = element_text(angle=90))+
+    labs(title = paste0(names[i]), x = "Year", y = "Estimate")
+
+  ggsave(paste0("C:/Users/zf53moho/Documents/NFDI4BioDiv/Data/Fish Data/Fischdaten_Datenbank/Red_List_Fish_Data/result_visualisation/",names[i],".png"), plot = p, dpi=300)
+
+  rm(year,tmp_dat,p,tmp_mod)
+}
 #### predict new values
 newdata <- data.frame(effort_m = 100, year_day = 250, factor_year=factor(c("2004", "2005", "2006", "2007", "2008", "2009", "2010", 
                                                                            "2011", "2012", "2013", "2014", "2015", "2016", "2017",
